415da178073a34818ffcd5e5b1f9cf1720228514
Carl Joseph <111431259+carljoseph25@users.noreply.github.com>
2024-12-05 22:42:18 +0800

    Updated Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -6,43 +6,33 @@ import java.awt.event.MouseEvent;
 
 public class Menu extends JPanel {
 
-    // Declaring the Game class for changing the scene
     final private Game game;
 
-    // Create an instance of ScoreFiles class to handle the .txt Files
     ScoreFiles scoreFiles = new ScoreFiles();
 
-    // Constructor of the Class
     public Menu(Game game){
         this.game = game;
 
-        // Layout to be used in this panel
         BoxLayout boxlayout = new BoxLayout(this, BoxLayout.Y_AXIS);
         this.setLayout(boxlayout);
 
-        // Called the method for adding the components in JPanel
         createGUI();
     }
 
-    // All the components are found here
     public void createGUI(){
 
-        // Declare the variables needed
         JLabel logoImage, scoreLabel, scoreText, playButton, playText;
 
-        // Setting up and Display the Guess the Number Logo Image
         logoImage = new JLabel(new ImageIcon("res/guess_the_number_logo.png"));
         logoImage.setBorder(new EmptyBorder(100, 0, 0, 0));
         logoImage.setAlignmentX(CENTER_ALIGNMENT);
         add(logoImage);
 
-        // Setting up and Display the High Score Label Image
         scoreLabel = new JLabel(new ImageIcon("res/high_score_label.png"));
         scoreLabel.setBorder(new EmptyBorder(35, 0, 0, 0));
         scoreLabel.setAlignmentX(CENTER_ALIGNMENT);
         add(scoreLabel);
 
-        // Setting up and Display the High Score
         scoreText = new JLabel(scoreFiles.showScore("scores/high_score.txt") + " points for " + scoreFiles.showGames("scores/high_score.txt") + " games");
         scoreText.setFont(new Font("Arial", Font.BOLD, 18));
         scoreText.setForeground(Color.WHITE);
@@ -50,7 +40,6 @@ public class Menu extends JPanel {
         scoreText.setAlignmentX(CENTER_ALIGNMENT);
         add(scoreText);
 
-        // Setting up and Display the Play Button
         playButton = new JLabel(new ImageIcon("res/play_button.png"));
         playButton.setBorder(new EmptyBorder(30, 0, 0, 0));
         playButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
@@ -58,14 +47,12 @@ public class Menu extends JPanel {
         linkPlay(playButton);
         add(playButton);
 
-        // Setting up and Display the "Click to Play" Text Image
         playText = new JLabel(new ImageIcon("res/click_play.png"));
         playText.setBorder(new EmptyBorder(20,0,0,0));
         playText.setAlignmentX(CENTER_ALIGNMENT);
         add(playText);
     }
 
-    // Method for Linking to Play Section
     public void linkPlay(JLabel jLabel){
         jLabel.addMouseListener(new MouseAdapter() {
             @Override
@@ -78,7 +65,6 @@ public class Menu extends JPanel {
         });
     }
 
-    // For changing the background of JPanel
     @Override
     protected void paintComponent(Graphics g) {
         g.drawImage(new ImageIcon("res/background.jpg").getImage(), 0, 0, null);
