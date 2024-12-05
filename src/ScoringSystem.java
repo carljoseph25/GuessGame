9191c9b3900b31c24b99cb9c701b25d331c5b570
Carl Joseph <111431259+carljoseph25@users.noreply.github.com>
2024-12-05 22:43:24 +0800

    Updated ScoringSystem.java

--- src/ScoringSystem.java
+++ src/ScoringSystem.java
@@ -1,24 +1,17 @@
 public class ScoringSystem {
-    // Declare score and attempts variables
     private int score, attempts;
 
-    // Assign the attempts 1 the moment this class called
     public ScoringSystem() {
         this.attempts = 1;
     }
-
-    // Return the score
     public int getScore() {
         return score;
     }
-
-    // Return the number of attempts
     public int getAttempts() {
         return attempts;
     }
 
-    // The Score System
-    // Add the points depending on the number of attempts
+
     public void scoreAttempt(){
         if(this.attempts == 1)
             this.score += 10;
@@ -32,7 +25,6 @@ public class ScoringSystem {
             this.score++;
     }
 
-    // Method for increasing an attempt
     public void incrementAttempt(){
         this.attempts++;
     }
