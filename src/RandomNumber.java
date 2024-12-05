3a52651ab57f52b0c60be7008166aa775a9ee960
Carl Joseph <111431259+carljoseph25@users.noreply.github.com>
2024-12-05 22:42:48 +0800

    Updated RandomNumber.java

--- src/RandomNumber.java
+++ src/RandomNumber.java
@@ -1,7 +1,7 @@
 public class RandomNumber {
     private final int min = 1, max = 50;
 
-    // Generate a Random Number
+    // 2
     public int generateNumber(){
         return (int)Math.floor(Math.random()*(max-min+1)+min);
     }
